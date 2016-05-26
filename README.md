b632e31b7bbb827d7e68c26ffdf1282e78b49673
Arpit Aggarwal <aggarwalarpit.89@gmail.com>
2016-05-26 21:13:57 +0400

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# karma-example
+
+### About Karma
+
+Karma - a test runner that fits all our needs. It is a tool that enables the running of source code (i.e. JavaScript) against real browsers via the CLI. The fact that it runs against real browsers rather than “fakes” with a virtual DOM is extremely powerful. DOM implementations vary across browsers therefore the idea is to use the actual browsers for correctness.
+
+
+### Karma Configuration file
+
+```
+karma init
+```
+
+http://karma-runner.github.io/0.13/config/configuration-file.html
+
+### Run Karma test
+
+```
+npm test
+```
+
+### References
+
+1. http://www.bradoncode.com/blog/2015/02/27/karma-tutorial/
+
+2. http://stackoverflow.com/questions/21855961/tutorial-and-resources-for-angularjs-karma
---
+++ test/calculator.test.js
@@ -0,0 +1,52 @@
+/*
+ * Unit tests for lib/calculator.js
+ */
+
+describe('Calculator', function() {
+
+  // inject the HTML fixture for the tests
+ //  HTML is injected into the browser (in the beforeEach block) for this test fixture.
+  beforeEach(function() {
+    var fixture = '<div id="fixture"><input id="x" type="text">' +
+      '<input id="y" type="text">' +
+      '<input id="add" type="button" value="Add Numbers">' +
+      'Result: <span id="result" /></div>';
+
+    document.body.insertAdjacentHTML(
+      'afterbegin',
+      fixture);
+  });
+
+  // remove the html fixture from the DOM
+  afterEach(function() {
+    document.body.removeChild(document.getElementById('fixture'));
+  });
+
+  // call the init function of calculator to register DOM elements
+  beforeEach(function() {
+    window.calculator.init();
+  });
+
+  it('should return 3 for 1 + 2', function() {
+    document.getElementById('x').value = 1;
+    document.getElementById('y').value = 2;
+    document.getElementById('add').click();
+    //The DOM elements can be accessed in the same way as working in real browser.
+    expect(document.getElementById('result').innerHTML).toBe('3');
+  });
+
+  it('should calculate zero for invalid x value', function() {
+    document.getElementById('x').value = 'hello';
+    document.getElementById('y').value = 2;
+    document.getElementById('add').click();
+    expect(document.getElementById('result').innerHTML).toBe('0');
+  });
+
+  it('should calculate zero for invalid y value', function() {
+    document.getElementById('x').value = 1;
+    document.getElementById('y').value = 'goodbye';
+    document.getElementById('add').click();
+    expect(document.getElementById('result').innerHTML).toBe('0');
+  });
+
+});
